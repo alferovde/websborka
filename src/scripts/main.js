772c79d4254b44525f75082fac9394c4ccd0d544
alferovde <dealferov1@yandex.ru>
2026-09-25 18:27:41 +0300

    continue project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('its work');
+import 'animate.css'
+
+const burger = document.querySelector('.burger');
+const menu_wrapper = document.querySelector('.mobile_menu__wrapper')
+
+burger.addEventListener('click', () => {
+    console.log(555);
+  burger.classList.toggle('active');
+
+  if(burger.classList.contains('active')){
+menu_wrapper.classList.remove('visible')
+  menu_wrapper.classList.add('animate__slideInDown')
+  }else{
+    menu_wrapper.classList.remove('animate__slideInDown')
+  menu_wrapper.classList.add('animate__slideOutUp')
+
+
+  setTimeout(()=>{
+ menu_wrapper.classList.add('visible')
+  },1000)
+
+  
+
+  }
+
+  setTimeout(()=>{
+    menu_wrapper.classList.remove('animate__slideOutUp')
+  menu_wrapper.classList.remove('animate__slideInDown')
+   
+  },1000)
+
+  
+
+});
+
+
+
